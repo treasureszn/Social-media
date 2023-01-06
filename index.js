ffb584559f7ffce6848abf775a45988d43a921ed
treasureszn <105241566+treasureszn@users.noreply.github.com>
2023-01-06 11:41:00 +0100

    made some changes to the post functionality

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const settingsMenu = document.querySelector('.settings-menu')
 const darkMode = document.getElementById('dark-btn')
+
+function settingsMenuToggle(){
+    settingsMenu.classList.toggle('settings-menu-height')
+}
+
+
+darkMode.onclick = function(){
+    darkMode.classList.toggle('drk-mode-btn')
+    document.body.classList.toggle('dark-mode')
+
+    if(localStorage.getItem('theme') = 'light'){
+        localStorage.setItem('theme',"dark");
+    }
+    else{
+        localStorage.setItem('theme',"light")
+    }
+}
