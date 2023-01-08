fadc7c52593e255387356e8438e0526028181ede
treasureszn <105241566+treasureszn@users.noreply.github.com>
2023-01-08 11:09:46 +0100

    bugs fixed

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const settingsMenu = document.querySelector('.settings-menu')
-const darkMode = document.getElementById('dark-btn')
+var settingsMenu = document.querySelector('.settings-menu')
+var darkBtn = document.getElementById('dark-btn')
 
 function settingsMenuToggle(){
     settingsMenu.classList.toggle('settings-menu-height')
 }
 
 
-darkMode.onclick = function(){
-    darkMode.classList.toggle('drk-mode-btn')
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle('dark-mode-btn')
     document.body.classList.toggle('dark-mode')
 
-    if(localStorage.getItem('theme') = 'light'){
+    if(localStorage.getItem('theme') == 'light'){
         localStorage.setItem('theme',"dark");
     }
     else{
         localStorage.setItem('theme',"light")
     }
 }
+if(localStorage.getItem('theme') == "light"){
+    darkBtn.classList.remove('dark-mode-btn')
+    document.body.classList.remove('dark-mode')
+}
+else if(localStorage.getItem('theme') == "dark"){
+    darkMode.classList.add('dark-mode-btn')
+    document.body.classList.add('dark-mode')
+}
+else{
+    localStorage.setItem('theme',"light");
+}
+
+
+
+localStorage.setItem('theme', 'light');
+localStorage.getItem('theme');
